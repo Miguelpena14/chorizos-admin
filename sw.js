19fdb56ad91b58f62d44575927d1f1275751f107
Miguelpena14 <m8kg6n5kgr@privaterelay.appleid.com>
2026-09-25 18:46:46 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,64 +1,22 @@
-const CACHE_NAME = "chorizos-admin-v6";
-
-const FILES_TO_CACHE = [
-  "./",
-  "./index.html",
-  "./manifest.json"
-];
+const CACHE = "chorizos-admin-v9";
+const FILES = ["./","./index.html","./manifest.json"];
 
 self.addEventListener("install", event => {
-
-  event.waitUntil(
-
-    caches.open(CACHE_NAME)
-      .then(cache =>
-        cache.addAll(FILES_TO_CACHE)
-      )
-
-  );
-
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(FILES)));
   self.skipWaiting();
-
 });
-
 
 self.addEventListener("activate", event => {
-
   event.waitUntil(
-
     caches.keys().then(keys =>
-
-      Promise.all(
-
-        keys
-          .filter(key => key !== CACHE_NAME)
-          .map(key => caches.delete(key))
-
-      )
-
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
     )
-
   );
-
   self.clients.claim();
-
 });
 
-
 self.addEventListener("fetch", event => {
-
   event.respondWith(
-
-    caches.match(event.request)
-      .then(cachedResponse => {
-
-        return (
-          cachedResponse ||
-          fetch(event.request)
-        );
-
-      })
-
+    caches.match(event.request).then(cached => cached || fetch(event.request))
   );
-
 });
